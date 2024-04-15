ac78abce32cc0ac8dddd7e3d9281f639c17ec6b3
Mohammad Nur Aldeen Obeidat <32016587+mnobeidat13@users.noreply.github.com>
2024-04-16 02:11:31 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const dice = document.querySelector('.dice');
-const rollBtn = document.querySelector('.roll');
+const dice = document.querySelector('#dice1');
+const rollBtn = document.querySelector('#roll1');
 
 var min = 1;
 var max = 8; // Adjusted max value for slower rotation
@@ -16,19 +16,70 @@ function getRandom(max, min) {
   return (Math.floor(Math.random() * (max-min)) + min) * 180; // Adjusted step size for slower rotation
 }
 
+function print(){
+    console.log("printed");
+}
 
-function flipCoin() {
-    var coin = document.getElementById('coin');
-    console.log('clicked')
+const dice2 = document.querySelectorAll('.dice2');
+const rollBtn2 = document.querySelector('#roll2');
+
+rollBtn2.onclick = function() {
+    dice2.forEach(die => {
+      const xRand = getRandom(max, min);
+      const yRand = getRandom(max, min);
+
+      die.style.webkitTransform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
+      die.style.transform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
+    });
+  }
+
+  function getRandom(max, min) {
+    return (Math.floor(Math.random() * (max - min)) + min) * 90;
+  }
+
+
+var coin = document.getElementById('coin');
+var flip = document.getElementById('flip')
+flip.addEventListener('click', () => {
     var flipResult = Math.random();
     coin.classList.remove('heads', 'tails');
     setTimeout(function(){
-      if (flipResult <= 0.5) {
+    if (flipResult <= 0.5) {
         coin.classList.add('heads');
         console.log('It is heads');
-      } else {
+    } else {
         coin.classList.add('tails');
         console.log('It is tails');
-      }
-    }, 100);
-  }
+    }
+    }, 50);
+})
+
+var coin1 = document.getElementById('coin1');
+var coin2 = document.getElementById('coin2');
+var flip2 = document.getElementById('flip2');
+
+flip2.addEventListener('click', () => {
+    var flipResult1 = Math.random();
+    var flipResult2 = Math.random();
+
+    coin1.classList.remove('heads', 'tails');
+    coin2.classList.remove('heads', 'tails');
+
+    setTimeout(function(){
+        if (flipResult1 <= 0.5) {
+            coin1.classList.add('heads');
+            console.log('Coin 1 is heads');
+        } else {
+            coin1.classList.add('tails');
+            console.log('Coin 1 is tails');
+        }
+
+        if (flipResult2 <= 0.5) {
+            coin2.classList.add('heads');
+            console.log('Coin 2 is heads');
+        } else {
+            coin2.classList.add('tails');
+            console.log('Coin 2 is tails');
+        }
+    }, 50);
+});
